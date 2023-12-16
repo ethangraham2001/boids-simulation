64724c812eba3929b0b00677243ef4abeffc8c46
Ethan <ethan.graham@epfl.ch>
2023-12-16 23:00:03 +0100

    updated main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,10 @@
+#include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "../inc/Environment.hpp"
 
-#define BG_PATH "sky.jpg"
+#define BG_PATH "assets/sky.jpg"
 
 /**
  * MAIN function. Calls all computation and draws the boids
@@ -13,17 +14,15 @@ int main()
     std::cout << "Intializing..." << std::endl;
     Environment env(800, 600, 1e-4, 1, 1);
 
-    env.add_random_boids(100);
+    env.add_random_boids(70);
 
     sf::RenderWindow window(sf::VideoMode(env.max_x, env.max_y), 
             "Boids Simulation");
 
     sf:sf::Texture t;
-    t.loadFromFile("sky.jpg");
+    t.loadFromFile(BG_PATH);
 
-    sf::CircleShape circle(100.f);
-    circle.setFillColor(sf::Color::Black);
-    circle.setPosition(300.f, 200.f);
+    sf::Sprite bg(t);
 
     while (window.isOpen())
     {
@@ -35,13 +34,15 @@ int main()
         }
         window.clear();
 
+        window.draw(bg);
+
         /* === UPDATE/DRAW BOIDS === */
         std::vector<Boid> boids = env.get_boids();
         for (auto b : boids)
         {
             // draw boids
-            sf::CircleShape c(2.f);
-            c.setFillColor(sf::Color::White);
+            sf::CircleShape c(5.f);
+            c.setFillColor(sf::Color::Black);
             Pos2D b_pos = b.get_position();
             c.setPosition(b_pos.get_x(), b_pos.get_y());
             window.draw(c);
